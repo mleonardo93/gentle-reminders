96a0209a314f46e66a1cadb4d22f312430d8c69b
mleonardo93 <mleonardo@gmail.com>
2018-09-18 23:39:38 -0400

    added splash screen

--- public/src/components/Splash.js
+++ public/src/components/Splash.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import SignUp from './SignUp'
+import Login from './Login'
+import './Splash.css'
 
 class Splash extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      login: false
+    }
+  }
+
+  handleLogin = () => {
+    this.setState({login: true});
+  }
+
+  handleSignUp = () => {
+    this.setState({login: false})
+  }
+
   render() {
     return(
-      <h1>Hello world!</h1>
+      <div>
+        <div className="Header">
+          <h1>Gentle Reminders</h1>
+          <h2>A kinder to-do app</h2>
+        </div>
+        <div className="Body">
+          <h3>
+            <button onClick={this.handleSignUp}>Sign up</button>
+            or 
+            <button onClick={this.handleLogin}>log in</button>
+          </h3> 
+          { this.state.login == false &&
+            <SignUp />
+          }
+          { this.state.login == true &&
+            <Login />
+          }
+        </div>
+      </div>
     );
   }
 }
